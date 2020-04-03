fc08574090f723e1c51154e1000ce3a62934ad6d
Joinn99 <joinnnor@outlook.com>
2020-04-03 17:35:35 +0800

    Update setup process

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-sudo apt install -y python3-pip
+sudo apt install -y python3-pip p7zip-full
 pip3 install -r requirements.txt
-wget -O Data/StackExpert.db https://docs.google.com/uc?id=1u1iTWKbG2v6TvxCRQHvgOnzBC0ib0N5K&export=download
+wget -O Data/StackExpert.7z https://docs.google.com/uc?id=1u1iTWKbG2v6TvxCRQHvgOnzBC0ib0N5K&export=download
+7za x Data/StackExpert.7z -oData
