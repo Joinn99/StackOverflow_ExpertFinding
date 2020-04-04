a5dba76e16f071e512a211e510bfd40cb3bbccc0
Joinn99 <joinnnor@outlook.com>
2020-04-04 17:23:36 +0800

    Update grid generate method

--- model/stackoptimizer.py
+++ model/stackoptimizer.py
@@ -110,13 +110,16 @@ class StackExpOptimizer():
         if grid_par['dtype'] == 'list':
             return grid_par['list']
         sc_par = {}
-        [sc_par['start'], sc_par['stop']] = np.sort(
-            np.random.uniform(**grid_par['range'], size=2))
+        while True:
+            [sc_par['start'], sc_par['stop']] = np.sort(
+                np.random.uniform(**grid_par['range'], size=2))
+            if sc_par['stop'] - sc_par['start'] >= grid_par['step']:
+                break
         if grid_par['scale'] == 'linear':
             grid = np.unique(np.linspace(
-                num=7, dtype=grid_par['dtype'], **sc_par))
+                num=9, dtype=grid_par['dtype'], **sc_par))
         else:
-            grid = np.geomspace(num=7, dtype=grid_par['dtype'], **sc_par)
+            grid = np.geomspace(num=9, dtype=grid_par['dtype'], **sc_par)
         return grid
 
     def _rec(self, clf_name, samp_name, res):
